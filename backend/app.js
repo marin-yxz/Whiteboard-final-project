4c1a8a745432f482e9bbe06fcb128923224c526f
marin-yxz <marin.grebenar.fax@gmail.com>
2022-06-21 20:47:26 +0200

    Tweak socket io server setup

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(
+  server,
+  {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+  },
+  { maxHttpBufferSize: 1e8, pingTimeout: 60000 },
+);
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  socket.on('chat', function (data) {
+    io.emit('message', data);
+  });
+  socket.on('canvas', function (data) {
+    socket.broadcast.emit('canvasState', data);
+  });
+});
+server.listen(8000);
